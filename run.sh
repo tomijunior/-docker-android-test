c3dc037056adf5b5e5f7fa9496c46988d6f77b70
Tomi Yahya <tomijunior@users.noreply.github.com>
2025-06-28 10:50:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash         
 
 if [ $# -eq 0 ]; then
-    docker compose -p automation-ai -f ./docker-compose.yml up -d
+    docker compose -p docker-android-automation -f ./docker-compose.yml up -d
 elif [ "$1" == "down" ] ;then
-    docker compose -p automation-ai -f ./docker-compose.yml down
+    docker compose -p docker-android-automation -f ./docker-compose.yml down
 fi
